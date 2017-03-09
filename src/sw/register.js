fd4ce0f4de08d36baebebac329fd9618704968c6
Patrick Hulce <patrick.hulce@gmail.com>
2017-03-08 22:06:24 -0800

    feat: disable service worker on dev by default

--- src/sw/register.js
+++ src/sw/register.js
@@ -1,12 +1,14 @@
 function register() {
   if (__HOT__) {
     console.warn('ServiceWorker not supported by HMR, turn off hot reloading to enable')
-  } else {
+  } else if (__PROD__ || localStorage.getItem('__forcesw__')) {
     navigator.serviceWorker.register('sw.js').then(function (registration) {
       console.log('ServiceWorker registration successful with scope: ', registration.scope)
     }).catch(function (err) {
       console.log('ServiceWorker registration failed: ', err)
     })
+  } else {
+    console.log('Skipping service worker registration, enable by setting localStorage[__forcesw__]')
   }
 }
 
