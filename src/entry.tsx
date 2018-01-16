6166d8a928c4cab38ed49213dc1cf35dd8007fd3
Patrick Hulce <patrick.hulce@gmail.com>
2018-01-15 18:34:51 -0800

    chore: fix tslint errors

--- src/entry.tsx
+++ src/entry.tsx
@@ -1,30 +1,30 @@
-/* tslint:disable */
-import './sw/register'
 import './styles/app.less'
+import './sw/register'
 
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-import {IS_DEV, INITIAL_STATE} from './env'
+import {INITIAL_STATE, IS_DEV} from './env'
 
+// tslint:disable
 const App = require('./containers/app').default
 const createStoreAndHistory = require('./state/store').default
+// tslint:enable
 
 document.body.classList.add('loaded')
 const reactRoot = document.getElementById('react-root')
 const {store, history} = createStoreAndHistory(INITIAL_STATE)
 
-function renderApp() {
+function renderApp(): void {
+  // tslint:disable-next-line
   const routes = require('./routes').default(store)
-  ReactDOM.render(
-    <App store={store} routes={routes} history={history} />,
-    reactRoot
-  )
+  ReactDOM.render(<App store={store} routes={routes} history={history} />, reactRoot)
 }
 
 let render = renderApp
 
 if (IS_DEV) {
+  // tslint:disable-next-line
   const RedBox = require('redbox-react').default
 
   render = () => {
--- src/env.tsx
+++ src/env.tsx
@@ -1,4 +1,3 @@
-/* tslint:disable */
 export const IS_DEV: boolean = !!(window as any).__DEV__
 export const IS_PROD: boolean = !!(window as any).__PROD__
-export const INITIAL_STATE: Object|undefined = (window as any).__INITIAL_STATE__
+export const INITIAL_STATE: object | undefined = (window as any).__INITIAL_STATE__
